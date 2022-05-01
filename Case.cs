4235066d357a0a49ac649bce581abc39c2d5165d
Naam Pondpat <naam.pt@gmail.com>
2022-05-01 09:38:11 -0400

    Initial commit

---
+++ Case.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebScraper
+{
+    public class Case
+    {
+        public Case(string attachmentID, string title, string downloadLink)
+        {
+            AttachmentID = attachmentID;
+            Title = title;
+            DownloadLink = downloadLink;
+        }
+
+
+        public string DownloadLink { get; set; }
+        public string AttachmentID { get; set; }
+
+        public string Title { get; set; }
+
+        public override string ToString()
+        {
+            var formatString = $"AttachmentID: {AttachmentID} \n" +
+                $"Title: {Title} \n" + 
+                $"DownloadLink: {DownloadLink} \n" ;
+
+            return formatString;
+        }
+    }
+}
